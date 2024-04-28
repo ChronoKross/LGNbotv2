9aa9cac471228d2ca3da51e7186d22e6a765647a
ChronoKross <nathanblainegwyn@gmail.com>
2024-04-27 23:06:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,6 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
-console.log("hello world");
+
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
